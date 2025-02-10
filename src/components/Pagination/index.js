3747392d06b31897ef691d8fdcc72ab614ab47a3
ananthusumanth <ananthusumanth1214@gmail.com>
2025-02-10 12:11:11 +0000

    moviedata

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -5,19 +5,12 @@ const Pagination = props => {
     totalposts,
     postPerPage,
     setpage,
-    setCurrentPage,
     currentPage,
     page,
     totalpages,
   } = props
   const pagePrev = page === 1 ? 1 : page - 1
   const pageNext = page === totalpages ? totalpages : page + 1
-
-  // const currentPagePrev = currentPage === 1 ? 1 : currentPage - 1
-  // const currentPageNext =
-  //   currentPage === Math.ceil(totalposts / postPerPage)
-  //     ? Math.ceil(totalposts / postPerPage)
-  //     : currentPage + 1
 
   const pagelist = []
   for (let i = 1; i <= Math.ceil(totalposts / postPerPage); i += 1) {
@@ -26,18 +19,6 @@ const Pagination = props => {
 
   return (
     <div className="main-pagination-container">
-      <div className="currentPageList">
-        {pagelist.map(each => (
-          <button
-            type="button"
-            key={each}
-            className={currentPage !== each ? 'inActive' : 'Active'}
-            onClick={() => setCurrentPage(each)}
-          >
-            {each}
-          </button>
-        ))}
-      </div>
       <div className="pageSection">
         <button
           type="button"
@@ -47,7 +28,7 @@ const Pagination = props => {
         >
           Prev
         </button>
-        <p className="currentPage">Page {page}</p>
+        <p className="currentPage">{page}</p>
         <button
           type="button"
           disabled={page === totalpages}
